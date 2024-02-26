00e94a605ebda75c19ed52efc3b5145b3d74e358
SimonMedic <szymonpaluch@hotmail.com>
2024-02-26 19:03:21 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const video = document.querySelector('iframe');
+const player = new VimeoPlayer(video);
+
+const throttledTimeUpdate = throttle(seconds => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  throttledTimeUpdate(data.seconds);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
