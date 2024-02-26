fb1e6ce1c5293b26af67e4997ea43f51ba042e06
SimonMedic <szymonpaluch@hotmail.com>
2024-02-26 18:41:58 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightBox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryElement = document.querySelector('.gallery');
+
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="gallery_item">
+<a href="${original}">
+<img class="gallery_image" src="${preview}" alt="${description}" />
+</a>
+</li>
+`
+  )
+  .join('');
+
+galleryElement.insertAdjacentHTML('beforeend', galleryList);
+
+let lightbox = new SimpleLightBox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
